3985175cb30493d79e84fe41646e9e44f84f4ff5
tlwpdus <tlwpdus@naver.com>
2020-04-07 10:10:26 +0900

    Update frame.py
    frame.py에 history라는 좋은 정보를 추가함

--- black_and_white/src/frame.py
+++ black_and_white/src/frame.py
@@ -1,5 +1,5 @@
 class PlayerVisibleState :
-    def __init__(self,rem,oppw,oppb,oppl,lopp,res,score,name) :
+    def __init__(self,rem,oppw,oppb,oppl,lopp,res,score,hist,name) :
         self.rem = rem # my remaining tiles in list
         self.white = oppw # opponent white tile remaining (includes self.opp)
         self.black = oppb # opponent black tile remaining (includes self.opp)
@@ -7,7 +7,8 @@ class PlayerVisibleState :
         self.last_opp = lopp # what oppenent drew in a last turn : black - 0, white - 1 
         self.res = res # result of last round : win - 1, lose - -1, draw - 0
         self.stage = 10 - len(rem) # current round
-        self.score = score
+        self.score = score # current score
+        self.history = hist # list of (my tile, opp tile color, result)
         self.name = name # Player1 or Player2
 
 class State :
@@ -17,12 +18,14 @@ class State :
         self.turn = 0
         self.last = 0
         self.lopp = [-1,-1]
+        self.history = []
     
     def draw(self, c1, c2) :
         if not c1 in self.rem[0] :
             raise Exception("Player 1 drew invalid tile : {} not in list".format(c1))
         if not c2 in self.rem[1] :
             raise Exception("Player 2 drew invalid tile : {} not in list".format(c2))
+        self.history.append((c1,c2))
         self.rem[0].remove(c1)
         self.rem[1].remove(c2)
         self.lopp = [c2%2, c1%2]
@@ -38,11 +41,17 @@ class State :
             self.last = 0
     
     def player_states(self) :
+        def res(a,b) :
+            if a>b:return 1
+            elif a<b:return -1
+            return 0
         w2 = sum(map(lambda x : x%2==1, self.rem[1]))
         b2 = len(self.rem[1])-w2
-        ps1 = PlayerVisibleState(self.rem[0],w2,b2,-1,self.lopp[0],-self.last,self.score,"Player1")
+        his1 = map(lambda p : (p[0],p[1]%2,res(p[0],p[1])),self.history)
+        ps1 = PlayerVisibleState(self.rem[0],w2,b2,-1,self.lopp[0],-self.last,self.score,his1,"Player1")
         w1 = sum(map(lambda x : x%2==1, self.rem[0]))
         b1 = len(self.rem[0])-w1
-        ps2 = PlayerVisibleState(self.rem[1],w1,b1,-1,self.lopp[1],self.last,self.score,"Player2")
+        ps2 = PlayerVisibleState(self.rem[1],w1,b1,-1,self.lopp[1],self.last,self.score,\
+                    map(lambda p : (p[1],p[0]%2,res(p[1],p[0])),self.history),"Player2")
         return (ps1,ps2)
 
